3149ea4810834453a207c695d0ae8bc6874d315b
Blur <pop297197228@163.com>
2025-01-12 17:50:30 +0900

    【Hero】 Hero init.

--- Source/Blur_ARPG_Samurai/Public/Gameplay/Characters/SamuraiCharacterBase.h
+++ Source/Blur_ARPG_Samurai/Public/Gameplay/Characters/SamuraiCharacterBase.h
@@ -4,13 +4,14 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Characters/BlurCharacterBase.h"
+#include "GameFramework/Characters/BlurPlayableCharacter.h"
 #include "SamuraiCharacterBase.generated.h"
 
 /**
  * 
  */
 UCLASS()
-class BLUR_ARPG_SAMURAI_API ASamuraiCharacterBase : public ABlurCharacterBase
+class BLUR_ARPG_SAMURAI_API ASamuraiCharacterBase : public ABlurPlayableCharacter
 {
 	GENERATED_BODY()
 	
